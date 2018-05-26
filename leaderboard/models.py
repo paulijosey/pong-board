3f389c5f0ee4f8a3c28d30af43c19a27453980b9
Scott Yargeau <scottyargeau@gmail.com>
2018-05-25 23:59:55 -0400

    Add models

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Player(models.Model):
+    """Table for keeping player information."""
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+
+
+class Match(models.Model):
+    """Table for keeping track of game scores and winners."""
+    winner = models.ForeignKey(Player, default=None, related_name='won_matches')
+    winning_score = models.IntegerField(default=None)
+    loser = models.ForeignKey(Player, default=None, related_name='lost_matches')
+    losing_score = models.IntegerField(default=None)
+    datetime = models.DateTimeField(default=timezone.now)
