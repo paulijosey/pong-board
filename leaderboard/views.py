e9cf2403323607f10edbe2ff8ee1e252bed4f7e6
Scott Yargeau <scottyargeau@gmail.com>
2018-05-28 21:39:41 -0400

    Pass match form into home page view and use in template

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
 from leaderboard.models import Match
+from leaderboard.forms import MatchForm
 
 
 def home_page(request):
     """Render view for home page."""
     recent_matches = Match.get_recent_matches(num_matches=20)
-    return render(request, 'home.html', {'recent_matches': recent_matches})
+    return render(
+        request,
+        'home.html',
+        {'recent_matches': recent_matches, 'form': MatchForm()}
+    )
