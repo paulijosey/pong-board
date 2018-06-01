01714b70f838ae665471f91d75799b3ec1ff03d3
Scott Yargeau <scottyargeau@gmail.com>
2018-06-01 03:01:01 -0400

    Modify home view to handle player form

--- leaderboard/tests/test_views.py
+++ leaderboard/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils.html import escape
 
 from leaderboard.models import Player, Match
-from leaderboard.forms import MatchForm
+from leaderboard.forms import MatchForm, PlayerForm, DUPLICATE_ERROR
 
 
 class HomePageTest(TestCase):
@@ -23,7 +23,16 @@ class HomePageTest(TestCase):
             'winning_score': 21,
             'losing_score': 24,
         }
+        self.valid_player_data = {
+            'first_name': 'Joe',
+            'last_name': 'Hope'
+        }
+        self.invalid_player_data = {
+            'first_name': 'Bob',
+            'last_name': 'Hope'
+        }
         self.match_submission_url = '/'
+        self.player_add_url = '/'
 
     def test_uses_template(self):
         """Test that the correct template is used."""
@@ -38,20 +47,20 @@ class HomePageTest(TestCase):
         )
         self.assertEqual(Match.objects.count(), 1)
 
-    def test_correct_form(self):
-        """Test that the correct form is used."""
+    def test_correct_match_form(self):
+        """Test that the correct match form is used."""
         response = self.client.get('/')
         self.assertIsInstance(response.context['match_form'], MatchForm)
 
-    def test_post_redirects(self):
-        """Test that the post redirects back to home page."""
+    def test_match_post_redirects(self):
+        """Test that match post redirects back to home page."""
         response = self.client.post(
             self.match_submission_url,
             self.valid_match_data
         )
         self.assertRedirects(response, '/')
 
-    def test_invalid_doesnt_save(self):
+    def test_invalid_match_doesnt_save(self):
         """Test that an invalid match submission doesn't save."""
         self.client.post(
             self.match_submission_url,
@@ -59,8 +68,8 @@ class HomePageTest(TestCase):
         )
         self.assertEqual(Match.objects.count(), 0)
 
-    def test_invalid_renders_error(self):
-        """Test that an invalid match submission renders error message."""
+    def test_invalid_match_renders_error(self):
+        """Test that invalid match submission renders error message."""
         response = self.client.post(
             self.match_submission_url,
             self.invalid_match_data
@@ -70,12 +79,62 @@ class HomePageTest(TestCase):
             'Losing score must be less than the winning score by at least 2 points.'
         )
 
-    def test_invalid_returns_form(self):
-        """Test that an invalid form is returned by the server."""
+    def test_invalid_match_returns_form(self):
+        """Test that invalid match form is returned by the server."""
         response = self.client.post(
             self.match_submission_url,
             self.invalid_match_data
         )
         form = response.context['match_form']
         expected_form = MatchForm(self.invalid_match_data)
+        self.assertEqual(form.as_p(), expected_form.as_p())
+
+    def test_player_add(self):
+        """Test that a player gets added and saved to database."""
+        self.client.post(
+            self.player_add_url,
+            self.valid_player_data
+        )
+        self.assertEqual(Player.objects.count(), 3)
+
+    def test_correct_player_form(self):
+        """Test that the correct form is used."""
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['player_form'], PlayerForm)
+
+    def test_player_post_redirects(self):
+        """Test that player post redirects back to home page."""
+        response = self.client.post(
+            self.player_add_url,
+            self.valid_player_data
+        )
+        self.assertRedirects(response, '/')
+
+    def test_invalid_player_doesnt_save(self):
+        """Test that invalid player submission doesn't save."""
+        self.client.post(
+            self.player_add_url,
+            self.invalid_player_data
+        )
+        self.assertEqual(Player.objects.count(), 2)
+
+    def test_invalid_player_renders_error(self):
+        """Test that invalid player submission renders error message."""
+        response = self.client.post(
+            self.player_add_url,
+            self.invalid_player_data
+        )
+        self.assertContains(
+            response,
+            DUPLICATE_ERROR
+        )
+
+    def test_invalid_player_returns_form(self):
+        """Test that an invalid player form is returned by the server."""
+        response = self.client.post(
+            self.player_add_url,
+            self.invalid_player_data
+        )
+        form = response.context['player_form']
+        expected_form = PlayerForm(self.invalid_player_data)
         self.assertEqual(form.as_p(), expected_form.as_p())
