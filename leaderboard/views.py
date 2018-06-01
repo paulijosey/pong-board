0770a0cb56bcf88ea98c63d245eb2c4de2024cdd
Scott Yargeau <scottyargeau@gmail.com>
2018-06-01 00:23:42 -0400

    Modify home page view and template to accept POST requests for match submission

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from leaderboard.models import Match
 from leaderboard.forms import MatchForm
@@ -7,8 +7,15 @@ from leaderboard.forms import MatchForm
 def home_page(request):
     """Render view for home page."""
     recent_matches = Match.get_recent_matches(num_matches=20)
+    if request.method == 'POST':
+        form = MatchForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = MatchForm()
     return render(
         request,
         'home.html',
-        {'recent_matches': recent_matches, 'form': MatchForm()}
+        {'recent_matches': recent_matches, 'form': form}
     )
