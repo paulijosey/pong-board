25e226cca24c27633fd490a17ad1cfe2ab23ec3e
Scott Yargeau <scottyargeau@gmail.com>
2018-06-03 20:05:17 -0400

    Add Player and Match tables to admin panel

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from leaderboard.models import Player, Match, PlayerRating
+
+admin.site.register(Player)
+admin.site.register(Match)
