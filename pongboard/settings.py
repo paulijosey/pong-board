e1945a97e67feee1cc8c8117981e3710b6985d12
Scott Yargeau <scottyargeau@gmail.com>
2018-06-04 00:05:41 -0400

    Modify settings to use db based on env

--- pongboard/settings.py
+++ pongboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+# Configure database according to env
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
