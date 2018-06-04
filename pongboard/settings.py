1bf8cb15540219e6ee89f60f0aadb50d4a792012
Scott Yargeau <scottyargeau@gmail.com>
2018-06-03 23:03:45 -0400

    Modify settings and URLs to allow for user authentication

--- pongboard/settings.py
+++ pongboard/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'pongboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            './templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
