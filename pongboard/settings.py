0aa80feeb6bedd6b8f53e31055243801e9d98617
Scott Yargeau <scottyargeau@gmail.com>
2018-06-03 23:50:59 -0400

    Change secret key and debug settings to work in prod too

--- pongboard/settings.py
+++ pongboard/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*=+e&8lvl7nq&#$#+)$x$s2@$ai*$z5-(154$%p6ax&)mel7dq'
+# Default: SECRET_KEY = '*=+e&8lvl7nq&#$#+)$x$s2@$ai*$z5-(154$%p6ax&)mel7dq'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', '*=+e&8lvl7nq&#$#+)$x$s2@$ai*$z5-(154$%p6ax&)mel7dq'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
