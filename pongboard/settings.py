0b8c48812f458b7b8a99b5b5636cbbfee5c39e04
Scott Yargeau <scottyargeau@gmail.com>
2018-06-04 00:22:32 -0400

    Modify static files root

--- pongboard/settings.py
+++ pongboard/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 # The absolute path to the directory where static files are located
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
