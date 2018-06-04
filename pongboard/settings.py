3880790a5915a4b324e0b1a6928f0795ae4adb16
Scott Yargeau <scottyargeau@gmail.com>
2018-06-04 00:53:54 -0400

    Update allowed hosts to work with Heroku

--- pongboard/settings.py
+++ pongboard/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'pingpongmastermind.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
